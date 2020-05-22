dc307e9d81cdecfc9d01e0be4d0be5d157184c58
Herbert Bintoroe <herbertbintoroe@gmail.com>
2020-05-22 13:31:41 +0700

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+const weatherForm = document.querySelector('#weatherForm')
+const search = document.querySelector('#weatherForm input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const fetchWeather = (address) => {
+  fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+  response.json().then((data)=> {
+    if (data.error) {
+      messageOne.textContent = data.error
+    } else {
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+    }
+  })
+})
+}
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  messageOne.textContent = 'Searching current weather for ' + location
+  messageTwo.textContent = ''
+
+
+  fetchWeather(location);
+})
